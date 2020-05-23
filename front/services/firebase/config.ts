f787c6439500f198a87233e0056a2da7711aa4fe
田辺悠介 <yusuke@yusuke-2.local>
2020-05-23 23:59:44 +0900

    initial commit

---
+++ front/services/firebase/config.ts
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyAPKHGEpxO7whxSA3sUADHkIdfIC3cGhQ8",
+  authDomain: "fanletter-208514.firebaseapp.com",
+  databaseURL: "https://fanletter-208514.firebaseio.com",
+  projectId: "fanletter-208514",
+  storageBucket: "fanletter-208514.appspot.com",
+  messagingSenderId: "217775245975"
+};
